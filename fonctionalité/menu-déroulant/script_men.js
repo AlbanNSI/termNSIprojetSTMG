16729782e0d0c7f88cd047c21276d414ef3d0ab4
Thierno <thierno.bah-diallo@saint-thom.fr>
2026-01-05 11:40:03 +0100

    menu-déroulant

--- fonctionalité/menu-déroulant/script_men.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  document.getElementById("menu-lateral").classList.toggle("open");
-  document.getElementById("overlay").classList.toggle("show");
-}
-
-function closeMenu() {
-  document.getElementById("menu-lateral").classList.remove("open");
-  document.getElementById("overlay").classList.remove("show");
-}
