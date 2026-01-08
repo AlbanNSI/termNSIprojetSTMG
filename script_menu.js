564b1ea4376ef75bd7631c32e4205df5b26eca3a
AlbanNSI <alban.guichard@saint-thom.fr>
2026-01-08 21:43:55 +0100

    si c pas le webhook c l'internet le probleme

--- script_menu.js
+++ script_menu.js
@@ -1,11 +1,13 @@
 function toggleMenu() {
   document.getElementById("menu-lateral").classList.toggle("open");
   document.getElementById("overlay").classList.toggle("show");
+  console.log("Toggle menu called");
 }
 
 function closeMenu() {
   document.getElementById("menu-lateral").classList.remove("open");
   document.getElementById("overlay").classList.remove("show");
+  console.log("Close menu called");
 }
 
 function scrollToTop() {
